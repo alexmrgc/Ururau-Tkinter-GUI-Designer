5328f8775e54806c7fc71de385f21963aed545c9
Alexandre Carneiro <alexmrgc@gmail.com>
2022-09-17 01:08:44 -0300

    Add files via upload
    todos os arquivos foram reformulados para utilizar ttk.Widgets.
    a janela principal agora possui um mainframe aonde todos os widgets são adicionados.

--- ururau.py
+++ ururau.py
@@ -5,11 +5,12 @@ from wgInspector import *
 
 if __name__=='__main__':
     Tk().withdraw()
-
+   
     inspector = WidgetInspector()
     window = WidgetWindow(inspector)
     toolbox = WidgetToolbox(window, inspector)
     window.set_toolbox(toolbox)
+    inspector.set_window(window)
 
     print('Ururau Tkinter GUI Designer ok')
 
