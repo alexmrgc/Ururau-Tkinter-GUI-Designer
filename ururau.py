2b7c8eb82ade80ae2771a1be2a82af8af7207b79
Alexandre Carneiro <alexmrgc@gmail.com>
2022-09-17 01:29:43 -0300

    Add files via upload

--- ururau.py
+++ ururau.py
@@ -5,13 +5,17 @@ from wgInspector import *
 
 if __name__=='__main__':
     Tk().withdraw()
-   
-    inspector = WidgetInspector()
-    window = WidgetWindow(inspector)
-    toolbox = WidgetToolbox(window, inspector)
-    window.set_toolbox(toolbox)
-    inspector.set_window(window)
 
+    inspector = WidgetInspector()
+    window = WidgetWindow()
+    toolbox = WidgetToolbox(window, inspector)
+
+    window.set_toolbox(toolbox)
+    inspector.set_toolbox(toolbox)
+
+    inspector.inspect_widget(window)
+
+    toolbox.mainloop()
     print('Ururau Tkinter GUI Designer ok')
 
         
